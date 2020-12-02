e830e54f6aa65561c5e14a7203b58ee5e2c9f96d
mihanchik94 <mihanchik94@gmail.com>
2020-12-02 12:19:24 +0300

    Отсортировать департаменты [#294671] (r.1.01)

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,14 +5,9 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int rsl = 0;
         String[] first = left.split("/");
         String[] second = right.split("/");
-        if (first[0].equals(second[0])) {
-            rsl = left.compareTo(right);
-        } else {
-            rsl = right.compareTo(left);
-        }
-        return rsl;
+        int rsl = second[0].compareTo(first[0]);
+        return rsl == 0 ? left.compareTo(right) : rsl;
     }
 }
--- src/test/java/ru/job4j/collection/DepDescCompTest.java
+++ src/test/java/ru/job4j/collection/DepDescCompTest.java
@@ -1,31 +1,66 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 public class DepDescCompTest {
 
     @Test
-    public void whenUpDepartmentGoBefore() {
-        int rsl = new DepDescComp().compare("K2", "K2/SK1");
-        assertTrue(rsl < 0);
+    public void compare() {
+        int rsl = new DepDescComp().compare(
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK1"
+        );
+        assertThat(rsl, greaterThan(0));
     }
 
     @Test
-    public void whenUpDepartmentGoAfter() {
-        int rsl = new DepDescComp().compare("K2/SK1", "K2");
-        assertTrue(rsl > 0);
+    public void whenUpDepartmentGoBefore() {
+        int rsl = new DepDescComp().compare(
+                "K2",
+                "K2/SK1"
+        );
+        assertThat(rsl, lessThan(0));
     }
 
     @Test
     public void whenDifferentDepartmentUp() {
-        int rsl = new DepDescComp().compare("K1/SK1", "K2/SK2");
-        assertTrue(rsl > 0);
+        int rsl = new DepDescComp().compare("K1/SK1", "K2/SK1");
+        assertThat(rsl, greaterThan(0));
     }
 
     @Test
     public void whenDifferentDepartmentDown() {
         int rsl = new DepDescComp().compare("K2/SK2/SSK1", "K1/SK2");
-        assertTrue(rsl < 0);
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public  void whenSortDesc() {
+        List<String> input = Arrays.asList(
+                "K2/SK1/SSK1",
+                "K2/SK1/SSK2",
+                "K1/SK1/SSK2",
+                "K1/SK1/SSK1",
+                "K1/SK2");
+        List<String> expect = Arrays.asList(
+                "K2",
+                "K2/SK1",
+                "K2/SK1/SSK1",
+                "K2/SK1/SSK2",
+                "K1",
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K1/SK2");
+        List<String> result = Departments.fillGaps(input);
+        Departments.sortDesc(result);
+        assertThat(result, is(expect));
+
     }
 }
