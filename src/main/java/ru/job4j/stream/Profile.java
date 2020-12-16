3881483940f4683cececeef535b34017c568900f
mihanchik94 <mihanchik94@gmail.com>
2020-12-16 20:23:15 +0300

    2. Список адресов [#294544]

---
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -0,0 +1,16 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Profile {
+    private Address address;
+
+    public Profile(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+}
+
---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+
+@Test
+public void collect() {
+        List<Profile> profiles = List.of(
+        new Profile(new Address("Moscow", "Street 1", 1, 1)),
+        new Profile(new Address("New York", "Street 2", 2, 2)),
+        new Profile(new Address("Novgorod", "Street 3", 3, 3)),
+        new Profile(new Address("New York", "Street 4", 4, 4))
+        );
+        List<Address> expected = List.of(
+        new Address("Moscow", "Street 1", 1, 1),
+        new Address("New York", "Street 2", 2, 2),
+        new Address("Novgorod", "Street 3", 3, 3),
+        new Address("New York", "Street 4", 4, 4)
+        );
+        assertThat(expected, is(new Profiles().collect(profiles)));
+        }
+
+}
