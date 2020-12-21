a36b40f1486f940a04cfc5aef9ea1754f78fc6e4
mihanchik94 <mihanchik94@gmail.com>
2020-12-21 20:41:00 +0300

    4. Преобразование List в Map. [#294546]

---
+++ src/main/java/ru/job4j/stream/Students2Map.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Students2Map {
+    public static Map<String, Student> convert(List<Student> students) {
+        return students.stream()
+                .distinct()
+                .collect(Collectors
+                        .toMap(Student::getSurname, student -> student, ((student, student2) -> student)));
+    }
+}
---
+++ src/test/java/ru/job4j/stream/Students2MapTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class Students2MapTest {
+
+    @Test
+    public void convertToMap() {
+        List<Student> students = List.of(
+                new Student(14, "Денисов"),
+                new Student(17, "Иванов"),
+                new Student(13, "Васякин"));
+        Map<String, Student> result = Students2Map.convert(students);
+        Map<String, Student> expected = Map.of(
+                "Денисов", new Student(14, "Денисов"),
+                "Иванов", new Student(17, "Иванов"),
+                "Васякин", new Student(13, "Васякин"));
+        assertThat(result, is(expected));
+    }
+}
