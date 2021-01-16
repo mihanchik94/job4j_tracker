46545a1bdde72efb0f77707e201fc68b48c3109c
mihanchik94 <mihanchik94@gmail.com>
2021-01-16 14:07:48 +0300

    Подготовка к экзамену, раздел Lambda: 1.2. Predicate. Проверить, что число четное

--- src/main/java/ru/job4j/lambda/PredicateCheckPositive.java
+++ src/main/java/ru/job4j/lambda/PredicateCheckPositive.java
@@ -5,11 +5,11 @@ import java.util.function.Predicate;
 //Нужно дописать лямбда выражение, которое проверяет, что число положительное
 
 public class PredicateCheckPositive {
-    public static boolean check (int num) {
+    public static boolean check(int num) {
         return check(a -> a >= 0, num);
     }
 
-    public static boolean check (Predicate<Integer> predicate, int num) {
+    public static boolean check(Predicate<Integer> predicate, int num) {
         return predicate.test(num);
     }
 }
